c9e97ca409d81d91a5dbae945cf48957c2fce26c
Max <mrc550email@gmail.com>
2023-09-01 09:49:08 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,21 @@
-const canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+function buttonClicked() {
+      console.log("daaaymmmmm")
+      
+			var image = document.getElementById("buttonImage");
 
-const ctx = canvas.getContext("2d");
+			// add the clicked class to apply the animation
+			image.classList.add("clicked");
 
-const particleSpeed = 30
-const particleOpacityChange = 0.05
-const particleSizeChange = 0.5
-
-function createParticle(x, y) {
-  const particle = {
-    x: x,
-    y: y,
-    size: Math.random() * 10 + 10,
-    vx: (Math.random() - 0.5) * particleSpeed,
-    vy: (Math.random() - 0.5) * particleSpeed,
-    opacity: 1
-  };
-  particles.push(particle);
+			// remove the clicked class after the animation is finished
+			setTimeout(function() {
+				image.classList.remove("clicked");
+			}, 300); // set the timeout to the same duration as the animation
+  
+			// add code here to get input from the button
+      
 }
 
-function drawParticles() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < particles.length; i++) {
-    const particle = particles[i];
-    ctx.beginPath();
-    ctx.arc(particle.x, particle.y, particle.size, 0, 2 * Math.PI);
-    ctx.fillStyle = "rgba(255, 255, 255, " + particle.opacity + ")";
-    ctx.fill();
-    particle.x += particle.vx;
-    particle.y += particle.vy;
-    particle.opacity -= particleOpacityChange;
-    particle.size -= particleSizeChange;
-    if (particle.opacity <= 0 || particle.size <= 0) {
-      particles.splice(i, 1);
-      i--;
-    }
-  }
-}
-
-const particles = [];
-
-canvas.addEventListener("click", function(event) {
-  for (let i = 0; i < 29; i++) {
-    createParticle(event.clientX, event.clientY);
-  }
-});
 
 requestAnimationFrame(function render() {
-  drawParticles();
   requestAnimationFrame(render);
 });
